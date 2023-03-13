9def861463b9df62a4433434cb0dd817d3f6d4b4
Anuj Maurya <anujmaurya@cedcoss.com>
2023-03-13 11:14:09 +0530

    first 10 subtasks completed

---
+++ JS/script.js
@@ -0,0 +1,53 @@
+// 1. Comment Your JavaScript Code
+
+// comment of type 1
+/* comment of type 2*/ 
+
+// 2. Declare JavaScript Variables
+var myName;
+
+// 3. Storing Values with the Assignment Operator
+var a;
+a = 7;
+
+// 4. Assigning the Value of One Variable to Another
+var a;
+a = 7;
+var b;
+b = a;
+
+// 5. Initializing Variables with the Assignment Operator
+var a = 9;
+
+// 6. Declare String Variables
+var myFirstName = "Anuj";
+var myLastName = "Maurya";
+
+// 7. Understanding Uninitialized Variables
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// 8. Understanding Case Sensitivity in Variables
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+// 9. Explore Differences Between the var and let Keywords
+let catName = "Oliver";
+let catSound = "Meow!";
+
+// 10. Declare a Read-Only Variable with the const Keyword
+const FCC = "freeCodeCamp"; // Change this line
+var fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
